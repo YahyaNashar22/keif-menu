91177aff7a1c64ef344766c242379bee07009ca9
YahyaNashar22 <yahyanashar22@gmail.com>
2024-12-16 13:15:44 +0200

    added back to top button

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "../App.css";
 import MenuSection from "../components/MenuSection";
 import menuJson from "../utils/menu.json";
@@ -9,10 +9,36 @@ const menu: MenuType = menuJson;
 
 const HomePage = () => {
   const [selectedCategory, setSelectedCategory] = useState<string>("breakfast");
+  const [showButton, setShowButton] = useState(false);
+
+  // Monitor scroll position to toggle visibility of the button
+  useEffect(() => {
+    const handleScroll = () => {
+      setShowButton(window.scrollY > 300);
+    };
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  // Scroll to the top
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
   return (
     <main className="home_page">
       <BreadCrumbs setSelectedCategory={setSelectedCategory} />
-      <MenuSection categoryName={selectedCategory} category={menu[selectedCategory]} />
+      <MenuSection
+        categoryName={selectedCategory}
+        category={menu[selectedCategory]}
+      />
+      {showButton && (
+        <button className="back_to_top" onClick={scrollToTop}>
+          ↑
+        </button>
+      )}
     </main>
   );
 };
